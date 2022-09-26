222d9e6da9cf39fd14ea0d1c099078dfc0e8df96
Daniel Kelly <me@danielkelly.io>
2022-09-26 12:09:33 -0500

    4-nuxtlink-for-navigation

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <nav style="display:flex; gap: 10px;">
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/movies">Movies</NuxtLink>
+      <NuxtLink to="https://vueschool.io">Vue School</NuxtLink>
+    </nav>
    <NuxtPage/>
   </div>
 </template>

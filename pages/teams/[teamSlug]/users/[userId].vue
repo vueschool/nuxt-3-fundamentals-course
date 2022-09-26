12964c0a8b537ff111fb888344a9a6e0492c7f7b
Daniel Kelly <me@danielkelly.io>
2022-09-26 13:04:29 -0500

    5-dynamic-route-params-in-nuxt-js

---
+++ pages/teams/[teamSlug]/users/[userId].vue
@@ -0,0 +1,10 @@
+<script setup>
+const route = useRoute();
+const message = computed(()=> `I'm on team ${route.params.teamSlug}
+    and my user id is ${route.params.userId}`)
+</script>
+<template>
+<p>
+    {{message}}
+</p>
+</template>

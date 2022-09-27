7589d86d74052540060d00212185c012fac18655
Daniel Kelly <me@danielkelly.io>
2022-09-27 15:41:47 -0500

    7-provide-a-single-root-element-for-page-components

--- pages/movies/index.vue
+++ pages/movies/index.vue
@@ -10,17 +10,19 @@ async function search() {
 </script>
 
 <template>
-  <form @submit.prevent="search">
-    <input type="text" v-model="query" />
-    <button>Search</button>
-  </form>
-  <ul style="display: flex; flex-wrap: wrap; gap: 10px; list-style: none">
-    <li v-for="movie in movies" :key="movie.imdbID">
-      <NuxtLink :to="{ name: 'movies-id', params: { id: movie.imdbID } }">
-        <img :src="movie.Poster" :alt="movie.title" width="100" />
-      </NuxtLink>
-    </li>
-  </ul>
+  <div>
+    <form @submit.prevent="search">
+      <input type="text" v-model="query" />
+      <button>Search</button>
+    </form>
+    <ul style="display: flex; flex-wrap: wrap; gap: 10px; list-style: none">
+      <li v-for="movie in movies" :key="movie.imdbID">
+        <NuxtLink :to="{ name: 'movies-id', params: { id: movie.imdbID } }">
+          <img :src="movie.Poster" :alt="movie.title" width="100" />
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped></style>

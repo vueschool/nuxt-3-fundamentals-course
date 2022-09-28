cc6679583d971f02b95df1ea8975b9bfe64fd3fe
Daniel Kelly <me@danielkelly.io>
2022-09-28 12:27:37 -0500

    8-create-components-that-auto-import

---
+++ components/movie/search.vue
@@ -0,0 +1,28 @@
+<script setup>
+const query = ref("");
+const movies = ref([]);
+async function search() {
+  const { Search } = await $fetch(
+    `http://www.omdbapi.com/?apikey=8e3f600b&s=${query.value}`
+  );
+  movies.value = Search;
+}
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="search">
+      <input type="text" v-model="query" />
+      <button>Search</button>
+    </form>
+    <ul style="display: flex; flex-wrap: wrap; gap: 10px; list-style: none">
+      <li v-for="movie in movies" :key="movie.imdbID">
+        <NuxtLink :to="{ name: 'movies-id', params: { id: movie.imdbID } }">
+          <img :src="movie.Poster" :alt="movie.title" width="100" />
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>

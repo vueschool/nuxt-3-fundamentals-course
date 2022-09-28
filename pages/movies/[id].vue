4f58996ce4fa2ea0d897e5a4c2ff359bda786bd8
Daniel Kelly <me@danielkelly.io>
2022-09-28 16:05:35 -0500

    11-configure-useasyncdata-with-options

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,8 +1,16 @@
 <script setup>
 const route = useRoute();
-const { data } = useAsyncData(`/movies/${route.params.id}`, () => {
-  return $fetch(`http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`);
-});
+const { data } = useAsyncData(
+  `/movies/${route.params.id}`,
+  () => {
+    return $fetch(
+      `http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`
+    );
+  },
+  {
+    pick: ["Plot", "Title"],
+  }
+);
 </script>
 <template>
   <div>

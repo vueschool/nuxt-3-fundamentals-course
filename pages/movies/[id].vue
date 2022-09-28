a5e36a5efba080dcb175b6715f1bc4c73c42a0ad
Daniel Kelly <me@danielkelly.io>
2022-09-28 14:36:56 -0500

    9-fetch-data-with-useayncdata

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,7 +1,13 @@
+<script setup>
+const route = useRoute();
+const { data } = useAsyncData(() => {
+  return $fetch(`http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`);
+});
+</script>
 <template>
-<div>
-    <h1>
-        {{$route.params.id}}
-    </h1>
-</div>
+  <div>
+    <pre>
+        {{ data }}
+    </pre>
+  </div>
 </template>

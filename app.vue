9bf8877984f5f57a81a73c10378d8e724fa9b6b7
Daniel Kelly <me@danielkelly.io>
2022-09-29 15:44:34 -0500

    14-call-the-usehead-composable-for-good-seo

--- app.vue
+++ app.vue
@@ -1,10 +1,17 @@
+<script setup>
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - My movie site` : "My movie site";
+  },
+});
+</script>
 <template>
   <div>
-    <nav style="display:flex; gap: 10px;">
+    <nav style="display: flex; gap: 10px">
       <NuxtLink to="/">Home</NuxtLink>
       <NuxtLink to="/movies">Movies</NuxtLink>
       <NuxtLink to="https://vueschool.io">Vue School</NuxtLink>
     </nav>
-   <NuxtPage/>
+    <NuxtPage />
   </div>
 </template>
--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,12 +1,10 @@
 <script setup>
 const route = useRoute();
 
-// {"Response":"False","Error":"Incorrect IMDb ID."}
-
-const { data } = useFetch(
+const { data } = await useFetch(
   `http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`,
   {
-    pick: ["Plot", "Title"],
+    pick: ["Plot", "Title", "Poster"],
     key: `/movies/${route.params.id}`,
     onResponse({ request, response }) {
       if (response._data.Error === "Incorrect IMDb ID.") {
@@ -15,6 +13,15 @@ const { data } = useFetch(
     },
   }
 );
+useHead({
+  title: data.value.Title,
+  meta: [
+    { name: "description", content: data.value.Plot },
+    { property: "og:description", content: data.value.Plot },
+    { property: "og:image", content: data.value.Poster },
+    { name: "twitter:card", content: `summary_large_image` },
+  ],
+});
 </script>
 <template>
   <div>

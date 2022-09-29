cd5786beeef85da7bc82597098903c753a077bbf
Daniel Kelly <me@danielkelly.io>
2022-09-29 13:59:19 -0500

    13-handle-usefetch-errors

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,10 +1,18 @@
 <script setup>
 const route = useRoute();
+
+// {"Response":"False","Error":"Incorrect IMDb ID."}
+
 const { data } = useFetch(
   `http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`,
   {
     pick: ["Plot", "Title"],
     key: `/movies/${route.params.id}`,
+    onResponse({ request, response }) {
+      if (response._data.Error === "Incorrect IMDb ID.") {
+        showError({ statusCode: 404, statusMessage: "Page Not Found" });
+      }
+    },
   }
 );
 </script>

72716059a83dd042f532201983079c83d1b38f2a
Daniel Kelly <me@danielkelly.io>
2023-08-23 14:34:59 -0500

    29-responsive-images

---
+++ pages/responsive.vue
@@ -0,0 +1,15 @@
+<script setup>
+definePageMeta({
+  layout: "plain",
+});
+</script>
+<template>
+  <NuxtImg src="/cat.jpg" sizes="xs:100vw sm:50vw md:1860px" />
+</template>
+
+<style scoped>
+img {
+  display: block;
+  max-width: 100%;
+}
+</style>
